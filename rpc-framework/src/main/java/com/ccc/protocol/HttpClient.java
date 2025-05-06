3fba5e3b8cb1fd1f15e501808432c49858e6ddc9
ccc <1329115652@qq.com>
2025-05-06 17:12:12 +0800

    refactor(rpc-framework): 优化 HttpClient 类的代码注释和逻辑
    - 增加了对 POST 请求为何需要启用输出流的解释
    - 添加了关于对象字节输出流用途的注释- 明确了 flush 方法的作用
    - 优化了输入流处理的注释，区分 InputStreamReader 和 BufferedReader 的作用

--- rpc-framework/src/main/java/com/ccc/protocol/HttpClient.java
+++ rpc-framework/src/main/java/com/ccc/protocol/HttpClient.java
@@ -20,19 +20,22 @@ public class HttpClient {
         // 通过java.net.HttpURLConnection发送HTTP请求，打开连接
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
-        // 启用输出流
+        // 因为 POST 请求通常需要在请求体中携带数据，所以需要启用输出流。
         connection.setDoOutput(true);
 
         // 将invocation对象序列化成字节数组，并写入到输出流中
         OutputStream outputStream = connection.getOutputStream();
+        // 对象字节输出流用于将java对象序列化为字节流，以便在网络传输中传输
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
         objectOutputStream.writeObject(invocation);
+        // flush方法触发发送网络请求
         objectOutputStream.flush();
         objectOutputStream.close();
 
         // 获取返回结果
         InputStream inputStream = connection.getInputStream();
-        // 使用BufferedReader将InputStream转换为字符串
+        // InputStreamReader将字节流转换为字符流
+        // 然后通过使用BufferedReader读取字符流数据
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         StringBuilder result = new StringBuilder();
         String line;
